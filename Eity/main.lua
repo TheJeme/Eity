af15847eb86a7e2796b68c3e57aa46f92fc1b604
Jeme <43149988+TheJeme@users.noreply.github.com>
2020-12-08 11:37:47 +0200

    Update main.lua

--- Eity/main.lua
+++ Eity/main.lua
@@ -18,7 +18,7 @@ function love.load()
   discordRPC.initialize(appId, true)
   now = os.time(os.date("*t"))
   detailsNow = "In Mainmenu"
-  stateNow = ""  
+  stateNow = ""
   nextPresenceUpdate = 0
 end
 
@@ -30,7 +30,7 @@ function discordApplyPresence()
     detailsNow = "In Mainmenu"
     stateNow = ""
   end
-  
+
   presence = {
     largeImageKey = "eity_icon",
     largeImageText = "Eity v1.0.0",
@@ -38,7 +38,7 @@ function discordApplyPresence()
     state = stateNow,
     startTimestamp = now,
   }
-  
+
   return presence
 end
 
@@ -47,30 +47,30 @@ function love.gamepadpressed(joystick, button)
 end
 
 function love.update(dt)
-  collectgarbage() 
+  collectgarbage()
   mx = love.mouse.getX() / simpleScale.getScale()
   my = love.mouse.getY() / simpleScale.getScale()
   scoreManager:update(dt)
   gameManager:update(dt)
   stateManager:update(dt)
-  
+
   if saveManager.settings.isEnabledVSync then
     love.window.setVSync(1)
   else
     love.window.setVSync(0)
   end
-  
+
   if nextPresenceUpdate < love.timer.getTime() then
       discordRPC.updatePresence(discordApplyPresence())
       nextPresenceUpdate = love.timer.getTime() + 2.0
   end
   discordRPC.runCallbacks()
-  
+
 
 end
 
 
-function love.draw()  
+function love.draw()
 	simpleScale.set()
     stateManager:draw()
     if saveManager.settings.isEnabledFPS then
